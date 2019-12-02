c4f7f0a403f80891b7b91efbbba6d90d2cb982e5
Alan Beaucheron <beaucheron.alanfree.fr>
2019-12-02 15:26:54 +0100

    template frontend

---
+++ view/frontend/Template.php
@@ -0,0 +1,115 @@
+<?php
+if (empty($_SESSION['id'])) {
+    $connected = false;
+} else {
+    $connected = true;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title ?> | Le site officiel de Jean Forteroche</title>
+    <meta name="description" content="<?= htmlspecialchars($metaDescription) ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- meta réseaux sociaux -->
+    <meta name="twitter:card" content="summary" />
+    <meta property="og:url" content="<?= $ogUrl ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="<?= $ogTitle ?>" />
+    <meta property="og:description" content="<?= htmlspecialchars($ogDescription) ?>" />
+    <!-- règles CSS et CDN -->
+    <link rel="stylesheet" href="public/css/frontend.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo:300,400,700|Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <!-- SEO JSON -->
+    <script type='application/ld+json'>
+        {
+            "@context": "http://www.schema.org",
+            "@type": "person",
+            "name": "Jean Forteroche",
+            "jobTitle": "Writer",
+            "url": "https://alanbeaucheron.ovh/projet4/",
+            "email": "email.com"
+        }
+    </script>
+    <script type='application/ld+json'>
+        {
+            "@context": "http://www.schema.org",
+            "@type": "WebSite",
+            "name": "<?= $ogTitle ?>",
+            "url": "<?= $ogUrl ?>"
+        }
+    </script>
+</head>
+
+<body>
+    <div id="bloc-page">
+        <nav id="navbar-example2" class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <div class="container">
+                <a class="navbar-brand text-white-50 text-uppercase" href="#">Jean Forteroche</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button>
+                <ul class="nav collapse navbar-collapse flex-sm-column flex-md-row justify-content-md-end align-items-sm-start" id="navbarToggler">
+                    <li class="nav-item">
+                        <a class="nav-link text-white text-uppercase" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white text-uppercase" href="#">Biographie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white text-uppercase" href="#">Billet simple</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white text-uppercase" href="#">Contact</a>
+                    </li>
+                    <?php if ($connected) { ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-white text-uppercase" href="#"><span class="fas fa-user-circle"></span> Admin</a>
+                        </li>
+                    <?php
+                }
+                ?>
+                </ul>
+            </div>
+        </nav>
+
+        <?= $content ?>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="text-center mb-5 col-8 offset-2 text-md-left col-md-4 offset-md-0">
+                        <h5 class="text-uppercase">Plan du site</h5>
+                        <div>
+                            <a href="#" class="text-white">Accueil</a><br />
+                            <a href="#" class="text-white">Biographie</a><br />
+                            <a href="#" class="text-white">Billet Simple</a><br />
+                            <a href="#" class="text-white">Contact</a>
+                        </div>
+                    </div>
+                    <div class="text-center mb-5 col-8 offset-2 text-md-left col-md-4 offset-md-0">
+                        <h5 class="text-uppercase">Admin</h5>
+                        <div>
+                            <a href="#" class="text-white">Se connecter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <?php if (!empty($script)) {
+        echo $script;
+    } ?>
+</body>
+
+</html>
