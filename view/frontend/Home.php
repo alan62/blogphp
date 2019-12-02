dc2ec8e4c33c406fc10e4b67a883023099a07001
Alan Beaucheron <beaucheron.alanfree.fr>
2019-12-02 21:04:22 +0100

    modification du design page d'accueil

--- view/frontend/Home.php
+++ view/frontend/Home.php
@@ -12,11 +12,10 @@ ob_start();
 ?>
 
 <header id="home-header">
-    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+    <div class="jumbotron p-4 p-md-5 text-white rounded bg-info">
+    <div class="col-md-12">
+      <h1 class="display-4 font-italic text-center">Un billet pour Alaska</h1>
+      <p class="lead my-3 text-center">Sur ce site vous aurez la possibilité de lire le roman chapitre par chapitre tout en ayant la possibilité de me donner votre ressenti par commentaire ou via le formulaire de contact.</p>
     </div>
   </div>
 </header>
@@ -27,10 +26,10 @@ ob_start();
         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
             <h3 class="mb-0">Chapitres en ligne</h3>
-            <p class="card-text mb-auto">Chaque chapitre sera publié en ligne .</p>
+            <p class="card-text mb-auto">Chaque chapitre du roman sera publié en ligne.</p>
         </div>
         <div class="col-auto d-none d-lg-block">
-            <img src="/public/images/roman-en-ligne.jpg">
+            <img class="img1 rounded-circle" src="http://alanbeaucheron.ovh/projet4/public/images/roman-en-ligne.jpg" alt="romanenligne">
         </div>
         </div>
     </div>
@@ -41,7 +40,7 @@ ob_start();
             <p class="mb-auto">Vous aurez la possibilité de commenter chaque chapitre pour donner votre point de vue sur le déroulement de l'histoire, des personnages...</p>
         </div>
         <div class="col-auto d-none d-lg-block">
-            <img src="/public/images/roman-interactif.jpg">       
+            <img class="img2 rounded-circle" src="http://alanbeaucheron.ovh/projet4/public/images/roman-interactif.jpg" alt="romaninteractif">       
             </div>
         </div>
     </div>
@@ -53,5 +52,5 @@ ob_start();
 <?php
 $content = ob_get_clean(); // fin du contenu de la variable $content
 // appel du template
-require('frontend.php');
+require 'template.php';
 ?>
