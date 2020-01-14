084cbfec8613c0ce019898dcff47a912e77bd1df
Alan Beaucheron <beaucheron.alanfree.fr>
2020-01-14 19:12:46 +0100

    ajout de l'état on_line pour les articles

--- model/Article.php
+++ model/Article.php
@@ -6,6 +6,7 @@ class Article {
     private $content;
     private $date_creation;
     private $date_update;
+    private $on_line;
 
 
     public function __construct(Array $data) // on oblige les paramètres à passer sous forme de tableau
@@ -36,6 +37,10 @@ class Article {
         {
             $this->setDate_update($data['date_update']);
         }
+        if (isset($data['on_line']))
+        {
+            $this->setOn_line($data['on_line']);
+        }
     }
 
     // GETTERS
@@ -60,6 +65,11 @@ class Article {
     {
         return $this->date_update;
     }
+    public function getOn_line()
+    {
+        return $this->on_line;
+    }
+
     
     // SETTERS
     
@@ -86,6 +96,11 @@ class Article {
     public function setDate_update($date_update)
     {
         $this->date_update = $date_update;
+        return $this;
+    }
+    public function setOn_line($on_line)
+    {
+        $this->on_line = $on_line;
         return $this;
     }
 }
