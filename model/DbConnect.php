1bea082b390c90ac50e51ff4857cab22c2eab542
Alan Beaucheron <beaucheron.alanfree.fr>
2020-01-20 22:42:49 +0100

    modif design background + commentaire pour la bdd

--- model/DbConnect.php
+++ model/DbConnect.php
@@ -6,7 +6,8 @@ Class DbConnect
 	{
 	    try
 	    { //nom de la base de donnée projet4 
-	       $this->db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+		   $this->db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', ''); 
+		   // new PDO('mysql:host=alanbeaucw175.mysql.db;dbname=alanbeaucw175;charset=utf8', 'alanbeaucw175', 'ascvfE159');
 	        return $this->db;
 	    }
 	    catch(Exception $e)
